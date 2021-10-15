1fd5a67dc392638d1142dbbf577cbcc7c2e39f26
HugoVictorDev <hugo.rocha@mercadolivre.com>
2021-10-15 18:24:02 -0300

    criando as entidades

--- src/main/java/com/meli/projetointegradorgroup1/ProjetoIntegradorGroup1Application.java
+++ src/main/java/com/meli/projetointegradorgroup1/ProjetoIntegradorGroup1Application.java
@@ -1,15 +1,19 @@
 package com.meli.projetointegradorgroup1;
 
-import lombok.Data;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+//Inicia o spring boot
 @SpringBootApplication
 public class ProjetoIntegradorGroup1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetoIntegradorGroup1Application.class, args);
     }
- //Eddie
+
+
+
 }
+
---
+++ src/main/java/com/meli/projetointegradorgroup1/entity/BatchStockItem.java
@@ -0,0 +1,10 @@
+package com.meli.projetointegradorgroup1.entity;
+
+//item do lote
+public class BatchStockItem {
+
+    public Product product;
+    private int quantity;
+    public BatchStoch batchStoch;
+
+}
