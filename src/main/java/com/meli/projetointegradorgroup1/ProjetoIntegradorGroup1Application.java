fddee3f7b1ef94c52dab0cf1b2dc136f1ba15d24
HugoVictorDev <hugo.rocha@mercadolivre.com>
2021-10-15 12:12:50 -0300

    iniciando projeto-integrador-group1

---
+++ src/main/java/com/meli/projetointegradorgroup1/ProjetoIntegradorGroup1Application.java
@@ -0,0 +1,15 @@
+package com.meli.projetointegradorgroup1;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProjetoIntegradorGroup1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoIntegradorGroup1Application.class, args);
+    }
+
+}
