4893542905f80becd2cfbddd6d767fd4b2dac179
msiqueira <marco.siqueira@mercadolivre.com>
2021-10-18 18:20:02 -0300

    atualização do arquivo data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,14 @@ VALUES ('lali', 'lela', 'lala', 'lala', 'lala'),
        ('luli', 'l9la', 'l0la', 'lal3', 'lal8');
 
 --------------------------------------------------------------------------------
+INSERT INTO PRODUCT (PRODUCT_ID, PRODUCT_NAME, MANUFACTURING_DATE, MANUFACTURING_TIME, DUE_DATE)
+VALUES ('lali', 'lela', 'lala', 'lala', 'lala'),
+       ('DSFH', 'LJH', 'UKHF', 'JGFF', 'JFGFJ'),
+       ('luli', 'l9la', 'l0la', 'lal3', 'lal8');
 
+
+INSERT INTO REPRESENTATIVE (NAME, CPF)
+VALUES ('IVO', '321.456.789.45');
 INSERT INTO REPRESENTATIVE (NAME, CPF)
 VALUES ('SILAS', '789.456.789.45'),
        ('IVO', '321.456.789.45');
