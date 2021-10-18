911fb210c35016a2b1cc51e0c254e57e120a7c0a
HugoVictorDev <hugo.rocha@mercadolivre.com>
2021-10-18 12:59:52 -0300

    refatorando as classes de entity em grupo

--- src/main/java/com/meli/projetointegradorgroup1/entity/Product.java
+++ src/main/java/com/meli/projetointegradorgroup1/entity/Product.java
@@ -1,4 +1,23 @@
 package com.meli.projetointegradorgroup1.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Product {
+
+    @Id
+    private String productId;
+    private String productName;
+
+    //data de producao
+    private String manufacturingDate;
+    //horario da producao
+    private String manufacturingTime;
+    //data de vencimento
+    private String dueDate;
+
+
 }
