ff1c4e002cec707115ef29ae14b43ed496b22b09
msiqueira <marco.siqueira@mercadolivre.com>
2021-10-18 15:59:32 -0300

    correção arquivo data.sql

--- src/main/java/com/meli/projetointegradorgroup1/entity/Product.java
+++ src/main/java/com/meli/projetointegradorgroup1/entity/Product.java
@@ -1,23 +1,25 @@
 package com.meli.projetointegradorgroup1.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Data
 @Entity
 @Table
 public class Product {
 
     @Id
-    private String productId;
-    private String productName;
-
+    private String product_Id;
+    private String product_Name;
     //data de producao
-    private String manufacturingDate;
+    private String manufacturing_Date;
     //horario da producao
-    private String manufacturingTime;
+    private String manufacturing_Time;
     //data de vencimento
-    private String dueDate;
+    private String due_Date;
 
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Product (product_Id, product_Name, manufacturing_Date, manufacturing_Time, due_Date)
+VALUES ('lali', 'lela', 'lala', 'lala', 'lala');
