3c23697ed7d35a13b795ef3af4a94dadd30612a9
HugoVictorDev <hugo.rocha@mercadolivre.com>
2021-10-18 13:50:04 -0300

    testando metodo de salvar produto no DB- OK

--- src/main/java/com/meli/projetointegradorgroup1/ProjetoIntegradorGroup1Application.java
+++ src/main/java/com/meli/projetointegradorgroup1/ProjetoIntegradorGroup1Application.java
@@ -1,15 +1,36 @@
 package com.meli.projetointegradorgroup1;
 
-import lombok.Data;
+
+import com.meli.projetointegradorgroup1.entity.Product;
+import com.meli.projetointegradorgroup1.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+//Inicia o spring boot
 @SpringBootApplication
-public class ProjetoIntegradorGroup1Application {
+public class ProjetoIntegradorGroup1Application  implements CommandLineRunner {
+
+    // instanciando o produto e a interface productRepository
+    @Autowired
+    ProductRepository productRepository;
+
+    Product produto1 = new Product("produto1", "produto2");
+
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetoIntegradorGroup1Application.class, args);
     }
-//Marco comment.
+
+    //RUNNN
+    @Override
+    public void run(String... args) throws Exception {
+
+        //salvando produto1 no banco de dados
+        productRepository.save(produto1);
+
+    }
 }
+
