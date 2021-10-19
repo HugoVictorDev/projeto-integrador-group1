0aabb306a960644fe77817cf10e4a118471637c1
Edenilson Teixeira <edenilson.paschoal@mercadolivre.com>
2021-10-18 22:08:00 -0300

    Insert nas bases e modelagem do MER

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -37,3 +37,10 @@ VALUES ('SILAS', '789.456.789.45'),
 
 INSERT INTO INBOUNDER (ORDER_NUMBER, ORDER_DATE)
 VALUES ('jkjkhjk', '8978989');
+--------------------------------------------------------------------------------
+
+INSERT INTO BATCHSTOCK (BATCHNUMBER, CURRENTTEMPERATURE, MINIMUMTEMPERATURE, CURRENTQUALITY)
+VALUES ('BATCHNUMBER', 'CURRENTTEMPERATURE', 'MINIMUMTEMPERATURE', 'CURRENTQUALITY')
+--------------------------------------------------------------------------------
+
+--INSERT INTO BATCHSTOCKITEM ();
