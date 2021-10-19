610d27c4877e971d061ff1f4cb6ae22ad30effd3
Patricia Souza <patricia.asouza@mercadolivre.com>
2021-10-19 19:48:14 -0300

    alteraçoes na classe Warehouse

--- src/main/java/com/meli/projetointegradorgroup1/entity/Warehouse.java
+++ src/main/java/com/meli/projetointegradorgroup1/entity/Warehouse.java
@@ -1,9 +1,35 @@
 package com.meli.projetointegradorgroup1.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
 
 
+@Entity
+@Data
+@Table(name = "warehouse")
 public class Warehouse {
 
-    private Representative represent;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "warehouseid")
+    private Long warehouseId;
 
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "size")
+    private String size;
+
+    public Warehouse() {
+    }
+
+    public Warehouse(String name, String address, String size) {
+        this.name = name;
+        this.address = address;
+        this.size = size;
+    }
 }
