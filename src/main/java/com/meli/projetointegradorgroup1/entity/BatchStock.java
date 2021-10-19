9b0c06a9754e9bd614dbe0ac22c15f110687e479
Edenilson Teixeira <edenilson.paschoal@mercadolivre.com>
2021-10-18 22:08:00 -0300

    Insert nas bases e modelagem do MER

--- src/main/java/com/meli/projetointegradorgroup1/entity/BatchStock.java
+++ src/main/java/com/meli/projetointegradorgroup1/entity/BatchStock.java
@@ -1,22 +1,63 @@
 package com.meli.projetointegradorgroup1.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Data
+@Entity
+@Table(name = "BATCHSTOCK")
 //conjunto de lote
 public class BatchStock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
+    @Column(name = "BATCHNUMBER")
     //numero do lote
     private String batchNumber;
+
     // temperatura atual
+    @Column(name = "CURRENTTEMPERATURE")
     private String currentTemprature;
+
     //temperatura minima
+    @Column(name = "MINIMUMTEMPERATURE")
     private String minimumTemprature;
+
     //estado inicial da qualidade do produto
+    @Column(name = "INITIALQUALITY")
     private String initialQuality;
+
     // estado atual da qualidade do produto
+    @Column(name = "CURRENTQUALITY")
     private String currentQuality;
+
+    @ManyToMany()
+    @JoinColumn(name = "REPRESENTATIVE_ID")
     private List<BatchStockItem> batchStockItems;
+
+    @OneToOne(orphanRemoval = true)
+    @JoinColumn(name = "batchstockitem_id")
+    private BatchStockItem batchstockitem;
+
+    public BatchStockItem getBatchstockitem() {
+        return batchstockitem;
+    }
+
+    public void setBatchstockitem(BatchStockItem batchstockitem) {
+        this.batchstockitem = batchstockitem;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
--- src/main/java/com/meli/projetointegradorgroup1/entity/BatchStockItem.java
+++ src/main/java/com/meli/projetointegradorgroup1/entity/BatchStockItem.java
@@ -1,10 +1,36 @@
 package com.meli.projetointegradorgroup1.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "BATCHSTOCKITEM")
 //item do lote
 public class BatchStockItem {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+
+    @OneToOne(mappedBy = "batchstockitem")
     public Product product;
+    @Column(name = "QUANTITY")
     private int quantity;
+
+    @OneToOne(mappedBy = "batchstockitem")
     public BatchStock batchStock;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BatchStockItem() {
+    }
 }
