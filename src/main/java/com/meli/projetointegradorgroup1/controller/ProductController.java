ccb72dee01196527090b2727edc9e76cc04746bc
Patricia Souza <patricia.asouza@mercadolivre.com>
2021-10-21 13:02:00 -0300

    alteraçoes em ProductController

--- src/main/java/com/meli/projetointegradorgroup1/controller/ProductController.java
+++ src/main/java/com/meli/projetointegradorgroup1/controller/ProductController.java
@@ -7,20 +7,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 
 @RestController
-@RequestMapping(path = "/produtos")
+@RequestMapping(path = "/product")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/listar")
-    public Iterable<Product> list(){
-        return productRepository.findAll();
-    }
 
-    @PostMapping("/criar")
+    // cadastrar novo produto
+    @PostMapping("/create")
     public ResponseEntity<Product> createProduct(@RequestBody Product product){
         try {
                 Product newProduct = productRepository.save(new Product(product.getProductName(), product.getManufacturingDate(), product.getManufacturingTime(), product.getDueDate()));
@@ -29,4 +28,51 @@ public class ProductController {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    // listar todos os produtos
+    @GetMapping("/list")
+    public Iterable<Product> list(){
+        return productRepository.findAll();
+    }
+
+    // buscar produto por id
+    @GetMapping("/list/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Long id){
+        Optional<Product> productFind = productRepository.findById(id);
+
+        if (productFind.isPresent()){
+            return new ResponseEntity<>(productFind.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // atualizar produto por id
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product){
+        Optional<Product> productFind = productRepository.findById(id);
+
+        if (productFind.isPresent()){
+            Product newProduct = productFind.get();
+            newProduct.setProductName(product.getProductName());
+            newProduct.setManufacturingDate(product.getManufacturingDate());
+            newProduct.setManufacturingTime(product.getManufacturingTime());
+            newProduct.setDueDate(product.getDueDate());
+            return new ResponseEntity<>(productRepository.save(newProduct), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // deletar produto por id
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable("id") Long id){
+        try {
+            productRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
