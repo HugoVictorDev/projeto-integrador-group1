ed45d222da34782f052745adb9fb573f506363e9
Edenilson Teixeira Paschoa <edenilson.paschoal@mercadolivre.com>
2021-10-25 17:56:21 -0300

    Inicio Persistence

---
+++ src/main/java/com/meli/projetointegradorgroup1/persistence/InboundOrderPersistence.java
@@ -0,0 +1,11 @@
+package com.meli.projetointegradorgroup1.persistence;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InboundOrderPersistence {
+
+    public InboundOrderPersistence(){
+
+    }
+}
