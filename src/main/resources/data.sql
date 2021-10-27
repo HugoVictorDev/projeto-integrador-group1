fdb41f5597a2679270e38bfb65ee0e7bdf53ee45
Edenilson Teixeira Paschoa <edenilson.paschoal@mercadolivre.com>
2021-10-27 10:02:30 -0300

    Projeto v0.0.2 b

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO PRODUCT (PRODUCT_NAME, MANUFACTURING_DATE, MANUFACTURING_TIME, DUE_DATE)
-VALUES ('lela', 'lala', 'lala', 'lala'),
-       ('LJH', 'UKHF', 'JGFF', 'JFGFJ'),
-       ('l9la', 'l0la', 'lal3', 'lal8');
+--INSERT INTO PRODUCT (PRODUCT_NAME, MANUFACTURING_DATE, MANUFACTURING_TIME, DUE_DATE)
+--VALUES ('lela', 'lala', 'lala', 'lala'),
+--       ('LJH', 'UKHF', 'JGFF', 'JFGFJ'),
+--       ('l9la', 'l0la', 'lal3', 'lal8');
 
 --------------------------------------------------------------------------------
 INSERT INTO WAREHOUSE (NAME, ADDRESS, SIZE)
@@ -14,14 +14,14 @@ VALUES ('SILAS', '789.456.789.45', 1),
        ('IVO', '321.456.789.45', 2);
 
 --------------------------------------------------------------------------------
-INSERT INTO BATCHSTOCK(ID, BATCH_NUMBER, CURRENT_QUALITY,  CURRENT_TEMPRATURE,  INITIAL_QUALITY,  MINIMUM_TEMPRATURE)
-VALUES (1, '23', '56', '88', '99', '98'),
-       (2, '23', '56', '88', '99', '98');
+INSERT INTO BATCHSTOCK(BATCH_NUMBER, CURRENT_QUALITY,  CURRENT_TEMPRATURE,  INITIAL_QUALITY,  MINIMUM_TEMPRATURE)
+VALUES (1, '56', '88', '99', '98'),
+       (2, '56', '88', '99', '98');
 
 --------------------------------------------------------------------------------
 INSERT INTO BATCHSTOCKITEMS (ID, QUANTITY, BATCHSTOCK_ID)
-VALUES (1, 1, 1),
-       (2, 2, 2);
+VALUES ( 1, 1),
+       ( 2, 2);
 
 --------------------------------------------------------------------------------
 
