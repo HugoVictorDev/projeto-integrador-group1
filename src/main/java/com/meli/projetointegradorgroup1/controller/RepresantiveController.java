ed78ade9f64b2f0e98ad24ae2682731f07ef4ea8
Patricia Souza <patricia.asouza@mercadolivre.com>
2021-11-04 17:51:15 -0300

    anotacoes swagger em representative

--- src/main/java/com/meli/projetointegradorgroup1/controller/RepresantiveController.java
+++ src/main/java/com/meli/projetointegradorgroup1/controller/RepresantiveController.java
@@ -5,6 +5,7 @@ import com.meli.projetointegradorgroup1.dto.RepresentativeDTO;
 import com.meli.projetointegradorgroup1.entity.Representative;
 import com.meli.projetointegradorgroup1.repository.RepresentativeRepository;
 import com.meli.projetointegradorgroup1.services.RepresentativeServices;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +27,7 @@ public class RepresantiveController {
 
     //Cadastrar representante
     @PostMapping("/post")
+    @ApiOperation(value = "Cadastrar novo representative")
     public RepresentativeDTO createRepresentative (@Valid @RequestBody RepresentativeDTO representativedto){
            representativeServices.valida(representativedto);
            Representative representative = RepresentativeDTO.converte(representativedto);
@@ -34,12 +36,14 @@ public class RepresantiveController {
 
     //Consultar lista de  representantes
     @GetMapping("/list")
+    @ApiOperation(value = "Retornar lista de representative")
     public List<RepresentativeDTO> getRepresentativeList() {
            return RepresentativeDTO.converte(representativeServices.listaRepresentative());
     }
 
     //Atualizar por id
     @PutMapping("/update/{id}")
+    @ApiOperation(value = "Atualizar representative a partir do id")
     public RepresentativeDTO updateRepresentative(@PathVariable("id") Long id,@Valid @RequestBody RepresentativeDTO representativedto) {
            Optional<Representative> representativeFind = representativeRepository.findById(id);
            Representative representative = representativeServices.validaUpdate(representativeFind, representativedto);
@@ -49,6 +53,7 @@ public class RepresantiveController {
 
     //consultar representatnte pelo ID
     @GetMapping("{id}")
+    @ApiOperation(value = "Retornar representative único a partir do id")
     public RepresentativeDTO getRepresentativeById(@PathVariable("id") Long id) {
            Optional<Representative> representativeFind = representativeRepository.findById(id);
            return RepresentativeDTO.converte(representativeServices.findRepresentative(representativeFind));
@@ -57,6 +62,7 @@ public class RepresantiveController {
 
     //deletar representatnte pelo ID
     @DeleteMapping("/delete/{id}")
+    @ApiOperation(value = "Deletar representative a partir do id")
     public RepresentativeDTO deleteRepresentativeById(@PathVariable("id") Long id) {
            Representative representative = representativeServices.findRepresentative(representativeRepository.findById(id));
            representativeRepository.deleteById(id);
