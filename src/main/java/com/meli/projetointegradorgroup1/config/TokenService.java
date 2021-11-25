458d4fd643a007232c83209adcc02f8cd5f594bf
Patricia Souza <patricia.asouza@mercadolivre.com>
2021-11-25 15:37:02 -0300

    alteraçao: aplicando autenticaçao

---
+++ src/main/java/com/meli/projetointegradorgroup1/config/TokenService.java
@@ -0,0 +1,55 @@
+package com.meli.projetointegradorgroup1.config;
+
+import com.meli.projetointegradorgroup1.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${jwt.expiration}")
+    private String expiration;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+
+    public String geraToken(Authentication authentication) {
+        User usuarioLogado = (User) authentication.getPrincipal();
+        Date hoje = new Date();
+        Long exp = new Long(expiration);
+        Date expiracao = new Date(hoje.getTime() + exp);
+
+        return Jwts.builder()
+                .setIssuer("Nossa APP")
+                .setSubject(usuarioLogado.getUser())
+                .setIssuedAt(hoje)
+                .setExpiration(expiracao)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+
+    public boolean tokenValido(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getUsername(String token) {
+        Jws<Claims> jwsClaims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+        Claims body = jwsClaims.getBody();
+        return body.getSubject();
+    }
+
+}
--- src/main/java/com/meli/projetointegradorgroup1/exception/ProdutoNaoCadastradoException.java
+++
@@ -1,8 +0,0 @@
-package com.meli.projetointegradorgroup1.exception;
-
-public class ProdutoNaoCadastradoException extends RuntimeException{
-
-    public ProdutoNaoCadastradoException(String mensagem){
-        super(mensagem);
-    }
-}
