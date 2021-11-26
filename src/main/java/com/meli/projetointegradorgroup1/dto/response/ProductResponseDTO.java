090afd603c8ff3c88a40cfa709bff9a69a3b4d15
msiqueira <marco.siqueira@mercadolivre.com>
2021-11-25 21:10:52 -0300

    Ultimos testes(Autenticação(Services) e InboundOrder(Controller/Services)

---
+++ src/main/java/com/meli/projetointegradorgroup1/dto/response/ProductResponseDTO.java
@@ -0,0 +1,29 @@
+package com.meli.projetointegradorgroup1.dto.response;
+import com.meli.projetointegradorgroup1.entity.Product;
+import com.meli.projetointegradorgroup1.entity.StockType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+/**
+ * @author Patricia Souza
+ */
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProductResponseDTO {
+
+    private String name;
+    private String description;
+    private StockType stockType;
+
+
+    public ProductResponseDTO(Product product) {
+        this.name = product.getName();
+        this.description = product.getDescription();
+        this.stockType = product.getStockType();
+    }
+
+}
